ed7d57ab525db629b531314da60aa105d51f0c02
GiattiChen <Jieting.17@intl.zju.edu.cn>
2020-08-17 15:26:42 +0800

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+ALLOWED_HOSTS = ['*']
 # Heroku设置
 if os.getcwd() == '/app':
     import dj_database_url
@@ -134,7 +135,8 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
     
     # 支持所有的主机头 (host header)
-    ALLOWED_HOSTS = ['0.0.0.0','localhost']
+#    ALLOWED_HOSTS = ['https','0.0.0.0','HTTP_X_FORWARDED_PROTO','127.0.0.1:8000']
+    ALLOWED_HOSTS = ['*']
     
     # 静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -143,3 +145,4 @@ if os.getcwd() == '/app':
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
+    
